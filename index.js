1080247cebfe2739752c2f18383b53af45211d29
Nicolas Dao <nic@neap.co>
2021-08-03 16:16:14 +1000

    feat: Add support for advanced search

--- index.js
+++ index.js
@@ -6,16 +6,33 @@
 const program = require('commander')
 const inquirer = require('inquirer')
 const clipboardy = require('clipboardy')
-const { awsManagedPolicies } = require('./src')
+const { aws } = require('./src')
 require('colors')
 const { version } = require('./package.json')
 program.version(version) // This is required is you wish to support the --version option.
 
+const NO_RESULTS_NEXT_STEP = 'No results... Press enter to move the next step.'
+
 inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'))
 
-const friendlyName = data => `${data.name} - ARN: ${data.arn}`
+const friendlyName = data => `${data.PolicyName} - ARN: ${data.Arn}`
 
 const choosePolicies = async (policies) => {
+	const { searchType } = await inquirer.prompt([{
+		type: 'list',
+		name: 'searchType',
+		message: 'How do you to search?',
+		choices: ['Text search', 'Advanced filter']
+	}])
+	
+	if (searchType == 'Text search')
+		return await textSearchPolicies(policies)
+	else 
+		return await advanceSearchPolicies(policies)
+
+}
+
+const textSearchPolicies = async (policies) => {
 	const { policy } = await inquirer.prompt([
 		{ 
 			type: 'autocomplete', 
@@ -25,19 +42,19 @@ const choosePolicies = async (policies) => {
 			source: function(answersSoFar, input) {
 				if (input) 
 					return policies
-						.filter(r => friendlyName(r).toLowerCase().indexOf(input.toLowerCase()) >= 0)
+						.filter(r => r.textSearch.indexOf(input.toLowerCase()) >= 0)
 						.map(r => ({
 							name: friendlyName(r),
 							value:r
 						}))
-						.sort((a,b) => a.value.name > b.value.name ? 1 : -1)
+						.sort((a,b) => a.value.PolicyName > b.value.PolicyName ? 1 : -1)
 				else
 					return policies
 						.map(r => ({
 							name: friendlyName(r),
 							value:r
 						}))
-						.sort((a,b) => a.value.name > b.value.name ? 1 : -1)
+						.sort((a,b) => a.value.PolicyName > b.value.PolicyName ? 1 : -1)
 			}
 		}
 	])
@@ -45,19 +62,177 @@ const choosePolicies = async (policies) => {
 	return policy
 }
 
+const advanceSearchPolicies = async (policies) => {
+	// Filter by effect
+	const { effect } = await inquirer.prompt([
+		{ 
+			type: 'list', 
+			name: 'effect', 
+			message: `Do you wish to ${'Allow'.bold} or ${'Deny'.bold} actions:`,
+			choices: ['Allow', 'Deny']
+		}
+	])
+	const effectPolicies = aws.filterPoliciesByEffect(policies, effect)
+
+	// Filter by actions
+	const { actions } = aws.getPoliciesPieces(effectPolicies)
+	const actionPolicies = await selectActions(effectPolicies, actions)
+	
+	// Filter by resources
+	const { resources } = aws.getPoliciesPieces(actionPolicies)
+	const resourcePolicies = await selectResources(actionPolicies, resources)
+
+	// Filter by text
+	return await textSearchPolicies(resourcePolicies)
+}
+
+const selectActions = async (policies, actions, selectedActions) => {
+	if (!selectedActions)
+		selectedActions = []
+
+	const choicesCount = actions.length
+	const { action } = await inquirer.prompt([
+		{ 
+			type: 'autocomplete', 
+			name: 'action', 
+			message: `Add any of the following ${choicesCount} action${choicesCount > 1 ? 's' : ''} to filter managed policies further or click 'next':`,
+			pageSize: 20,
+			source: function(answersSoFar, input) {
+				let results = actions
+				if (input)
+					results = actions.filter(r => r.toLowerCase().indexOf(input.toLowerCase()) >= 0)
+
+				if (results.length)
+					return ['next', ...results.sort((a,b) => a > b ? 1 : -1)]
+				else
+					return [NO_RESULTS_NEXT_STEP]
+			}
+		}
+	])
+
+	if (action == 'next' || action == NO_RESULTS_NEXT_STEP)
+		return policies
+	else {
+		selectedActions.push(action)
+		const newPolicies = aws.filterPoliciesByActions(policies, selectedActions)
+		const { actions:updatedActions } = aws.getPoliciesPieces(newPolicies)
+		const canSelectMoreActions = updatedActions.some(a => selectedActions.indexOf(a) < 0)
+		if (canSelectMoreActions)
+			return await selectActions(newPolicies, updatedActions, selectedActions)
+		else
+			return newPolicies
+	}
+}
+
+const selectResources = async (policies, resources, selectedResources) => {
+	if (!selectedResources)
+		selectedResources = []
+
+	const choicesCount = resources.length
+	const { resource } = await inquirer.prompt([
+		{ 
+			type: 'autocomplete', 
+			name: 'resource', 
+			message: `Add any of the following ${choicesCount} resource${choicesCount > 1 ? 's' : ''} to filter managed policies further or click 'next':`,
+			pageSize: 20,
+			source: function(answersSoFar, input) {
+				let results = resources
+				if (input)
+					results = resources.filter(r => r.toLowerCase().indexOf(input.toLowerCase()) >= 0)
+
+				if (results.length)
+					return ['next', ...results.sort((a,b) => a > b ? 1 : -1)]
+				else
+					return [NO_RESULTS_NEXT_STEP]
+			}
+		}
+	])
+
+	if (resource == 'next')
+		return policies
+	else {
+		selectedResources.push(resource)
+		const newPolicies = aws.filterPoliciesByResources(policies, selectedResources)
+		const { resources:updatedResources } = aws.getPoliciesPieces(newPolicies)
+		const canSelectMoreResources = updatedResources.some(a => selectedResources.indexOf(a) < 0)
+		if (canSelectMoreResources)
+			return await selectResources(newPolicies, updatedResources, selectedResources)
+		else
+			return newPolicies
+	}
+}
+
+const selectPolicy = async () => {
+	const policies = await aws.managedPolicies
+	const policy = await choosePolicies(policies)
+	
+	clipboardy.writeSync(policy.Arn)
+	
+	console.log(`${policy.Arn.bold} copied to your clipboard`.green)
+	console.log('')
+
+	const nextStep01 = `Inspect ${policy.PolicyName}`
+	const nextStep02 = 'Select another policy'
+	const nextStep03 = 'No, thanks. I\'m done for now.'
+	const nextStep04 = 'Copy this policy to clipboard.'
+	const { nextStep } = await inquirer.prompt([
+		{ 
+			type: 'list', 
+			name: 'nextStep', 
+			message: 'Do you wish to carry on with the following?',
+			pageSize: 20,
+			choices:[nextStep01, nextStep02, nextStep03]
+		}
+	])
+
+	if (nextStep == nextStep01) {
+		/*eslint-disable */
+		const { textSearch, ...rest } = JSON.parse(JSON.stringify(policy))
+		/*eslint-enable */
+		const policyStr = JSON.stringify(rest,null, '	')
+		console.log(policyStr)
+		console.log('')
+		const resp = await inquirer.prompt([
+			{ 
+				type: 'list', 
+				name: 'nextStep', 
+				message: 'Do you wish to carry on with the following?',
+				pageSize: 20,
+				choices:[nextStep04, nextStep02, nextStep03]
+			}
+		])
+		if (resp.nextStep == nextStep02)
+			return await selectPolicy()
+		else if (resp.nextStep == nextStep04) {
+			clipboardy.writeSync(policyStr)
+			console.log('Policy content copied to your clipboard'.green)
+			const resp02 = await inquirer.prompt([
+				{ 
+					type: 'list', 
+					name: 'nextStep', 
+					message: 'Do you wish to carry on with the following?',
+					pageSize: 20,
+					choices:[nextStep02, nextStep03]
+				}
+			])
+			if (resp02.nextStep == nextStep02)
+				return await selectPolicy()
+			else 
+				return
+		} else 
+			return
+	} else if (nextStep == nextStep02)
+		return await selectPolicy()
+	else
+		return
+}
+
 // 1. Creates your first command. This example shows an 'order' command with a required argument
 // called 'product' and an optional argument called 'option'.
 program
 	.command('select')
 	.description('Default behavior. Lists/searches the AWS managed policies and copy the selected one to the clipboard. Equivalent to `npx get-policies`') // Optional description
-	.action(async () => {
-		const policies = await awsManagedPolicies
-		const policy = await choosePolicies(policies)
-
-		clipboardy.writeSync(policy.arn)
-
-		console.log(`${policy.arn.bold} copied to your clipboard`.green)
-	})
+	.action(selectPolicy)
 
 // 2. Deals with cases where no command is passed.
 const cmdArgs = [process.argv[0], process.argv[1]]
--- src/index.js
+++ src/index.js
@@ -1,6 +1,155 @@
 const https = require('https')
 
-const GIST = 'https://gist.githubusercontent.com/nicolasdao/c54338247534c7d32f6dd21b045cb170/raw'
+const GIST = 'https://gist.githubusercontent.com/nicolasdao/ce55f5936113fc5a3b1badb8d4adee9d/raw'
+
+/**
+ * Gets all the actions from a policiy document.
+ * 
+ * @param  {[String]} statement[].Action
+ * 
+ * @return {[String]} actions
+ */
+const getActions = statement => {
+	if (Array.isArray(statement))
+		return statement.reduce((acc,s) => {
+			if (s && s.Action) {
+				if (Array.isArray(s.Action))
+					acc.push(...s.Action)
+				else if (typeof(s.Action) == 'string')
+					acc.push(s.Action)
+			}
+			return acc
+		},[])
+	else if (typeof(statement) == 'object' && statement.Action) {
+		if (Array.isArray(statement.Action))
+			return statement.Action
+		else if (typeof(statement.Action) == 'string')
+			return [statement.Action]
+		else
+			return []
+	}
+}
+
+const getPoliciesPieces = policies => {
+	const { actions, resources } = policies.reduce((acc,policy) => {
+		if (policy && policy.Document && policy.Document.Statement) {
+			// Get actions
+			const actions = getActions(policy.Document.Statement)
+			actions.forEach(action => acc.actions[action] = true)
+
+			// Get resources
+			if (Array.isArray(policy.Document.Statement))
+				policy.Document.Statement.forEach(s => {
+					if (Array.isArray(s.Resource))
+						s.Resource.forEach(r => r ? acc.resources[r] = true : null)
+					else if (s.Resource)
+						acc.resources[s.Resource] = true
+				})
+			else if (policy.Document.Statement.Resource)
+				if (Array.isArray(policy.Document.Statement.Resource))
+					policy.Document.Statement.Resource.forEach(r => r ? acc.resources[r] = true : null)
+				else if (policy.Document.Statement.Resource)
+					acc.resources[policy.Document.Statement.Resource] = true	
+		}
+		return acc
+	}, { actions:{}, resources:{} })
+
+	return {
+		actions: Object.keys(actions).sort((a,b) => a < b ? -1 : 1),
+		resources: Object.keys(resources).sort((a,b) => a < b ? -1 : 1)
+	}
+}
+
+const filterPoliciesByEffect = (policies, effect) => {
+	if (!effect)
+		return policies||[]
+
+	return (policies||[]).reduce((acc,policy) => {
+		if (policy && policy.Document && policy.Document.Statement) {
+			if (Array.isArray(policy.Document.Statement)) {
+				const matchingStatements = policy.Document.Statement.filter(s => s.Effect == effect)
+				if (matchingStatements[0]) {
+					const newPolicy = JSON.parse(JSON.stringify(policy))
+					newPolicy.Document.Statement = matchingStatements
+					acc.push(newPolicy)	
+				}
+			} else if (policy.Document.Statement.Effect == effect)
+				acc.push(policy)				
+		}
+
+		return acc
+	}, [])
+}
+
+const filterPoliciesByActions = (policies, actions) => {
+	if (!actions || !actions.length)
+		return policies||[]
+
+	return (policies||[]).reduce((acc,policy) => {
+		if (policy && policy.Document && policy.Document.Statement) {
+			if (Array.isArray(policy.Document.Statement)) {
+				const matchingStatements = policy.Document.Statement.filter(s => {
+					if (Array.isArray(s.Action))
+						return actions.every(a => s.Action.indexOf(a) >= 0)
+					else if (typeof(s.Action) == 'string')
+						return actions.every(a => a == s.Action)
+					else
+						return false
+				})
+				if (matchingStatements[0]) {
+					const newPolicy = JSON.parse(JSON.stringify(policy))
+					newPolicy.Document.Statement = matchingStatements
+					acc.push(newPolicy)	
+				}
+			} else if (policy.Document.Statement.Action) {
+				const matches = Array.isArray(policy.Document.Statement.Action)
+					? actions.every(a => policy.Document.Statement.Action.indexOf(a) >= 0)
+					: typeof(policy.Document.Statement.Action) == 'string'
+						? actions.every(a => a == policy.Document.Statement.Action)
+						: false
+				if (matches)
+					acc.push(policy)		
+			}		
+		}
+
+		return acc
+	}, [])
+}
+
+const filterPoliciesByResources = (policies, resources) => {
+	if (!resources || !resources.length)
+		return policies||[]
+
+	return (policies||[]).reduce((acc,policy) => {
+		if (policy && policy.Document && policy.Document.Statement) {
+			if (Array.isArray(policy.Document.Statement)) {
+				const matchingStatements = policy.Document.Statement.filter(s => {
+					if (Array.isArray(s.Resource))
+						return resources.every(a => s.Resource.indexOf(a) >= 0)
+					else if (typeof(s.Resource) == 'string')
+						return resources.every(a => a == s.Resource)
+					else
+						return false
+				})
+				if (matchingStatements[0]) {
+					const newPolicy = JSON.parse(JSON.stringify(policy))
+					newPolicy.Document.Statement = matchingStatements
+					acc.push(newPolicy)	
+				}
+			} else if (policy.Document.Statement.Resource) {
+				const matches = Array.isArray(policy.Document.Statement.Resource)
+					? resources.every(a => policy.Document.Statement.Resource.indexOf(a) >= 0)
+					: typeof(policy.Document.Statement.Resource) == 'string'
+						? resources.every(a => a == policy.Document.Statement.Resource)
+						: false
+				if (matches)
+					acc.push(policy)		
+			}		
+		}
+
+		return acc
+	}, [])
+}
 
 const httpGet = url => new Promise((next, fail) => https.get(url, res => {
 	let data = []
@@ -10,14 +159,28 @@ const httpGet = url => new Promise((next, fail) => https.get(url, res => {
 	})
 
 	res.on('end', () => {
-		next(JSON.parse(Buffer.concat(data).toString()))
+		const policies = Object.entries(JSON.parse(Buffer.concat(data).toString())).map(([,policy]) => {
+			if (!policy)
+				policy = {}
+
+			policy.textSearch = `${policy.Arn} ${policy.PolicyName} ${policy.PolicyId}`.toLowerCase()
+			return policy
+		})
+
+		next(policies)
 	})
 }).on('error', err => {
 	fail(err)
 }))
 
-const awsManagedPolicies = httpGet(`${GIST}/aws_managed_policies.json`)
+const awsManagedPolicies = httpGet(`${GIST}/all_aws_managed_policies.json`)
 
 module.exports = {
-	awsManagedPolicies
+	aws: {
+		managedPolicies: awsManagedPolicies,
+		filterPoliciesByEffect,
+		filterPoliciesByActions,
+		filterPoliciesByResources,
+		getPoliciesPieces
+	}
 }
